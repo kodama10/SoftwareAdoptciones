467c7b0d0597bf38d1ebb626f822de9b5810acaa
kodama10 <168107292+kodama10@users.noreply.github.com>
2024-10-14 20:29:40 -0300

    Update models.py

--- software_adopcion_refugio/adopcion/models.py
+++ software_adopcion_refugio/adopcion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    especie = models.CharField(max_length=50, choices=[('perro', 'Perro'), ('gato', 'Gato')])
+    raza = models.CharField(max_length=100)
+    estado_salud = models.TextField()
+    comportamiento = models.TextField()
+    foto = models.ImageField(upload_to='fotos_animales/')
+    disponible = models.BooleanField(default=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.especie})"
