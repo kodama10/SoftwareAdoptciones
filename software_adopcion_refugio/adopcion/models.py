6190e1678ef93d5b23b64ce12a423b5482211541
kodama10 <168107292+kodama10@users.noreply.github.com>
2024-10-14 20:31:19 -0300

    Update models.py

--- software_adopcion_refugio/adopcion/models.py
+++ software_adopcion_refugio/adopcion/models.py
@@ -13,3 +13,19 @@ class Animal(models.Model):
 
     def __str__(self):
         return f"{self.nombre} ({self.especie})"
+
+class SolicitudAdopcion(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    nombre_adoptante = models.CharField(max_length=100)
+    email_adoptante = models.EmailField()
+    telefono_adoptante = models.CharField(max_length=20)
+    direccion = models.TextField()
+    estado = models.CharField(
+        max_length=20,
+        choices=[('en revisión', 'En Revisión'), ('aprobada', 'Aprobada'), ('rechazada', 'Rechazada')],
+        default='en revisión'
+    )
+    fecha_solicitud = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Solicitud de {self.nombre_adoptante} para {self.animal.nombre}"
